f2bc95c97df5e2b09b1e7b50c82f64665a9603d9
Li Chaoyi <icylydia@gmail.com>
2025-05-28 16:46:39 +0900

    fix: update text label on container form

--- src/components/container/ContainerForm.vue
+++ src/components/container/ContainerForm.vue
@@ -26,14 +26,14 @@ const { formModel: container } = storeToRefs(useContainerStore());
         <q-item class="col-12">
             <q-item-section>
                 <q-input :model-value="container.containerNumber"
-                    @update:model-value="container.containerNumber = ($event as string || undefined)" label="集装箱号"
+                    @update:model-value="container.containerNumber = ($event as string || undefined)" label="コンテナ番号"
                     :readonly="readonly" />
             </q-item-section>
         </q-item>
         <q-item class="col-12">
             <q-item-section>
                 <q-input :model-value="container.shippingLine"
-                    @update:model-value="container.shippingLine = ($event as string || undefined)" label="船公司"
+                    @update:model-value="container.shippingLine = ($event as string || undefined)" label="船会社"
                     :readonly="readonly" />
             </q-item-section>
         </q-item>
@@ -47,50 +47,50 @@ const { formModel: container } = storeToRefs(useContainerStore());
         <q-item class="col-12">
             <q-item-section>
                 <q-input :model-value="container.voyageNumber"
-                    @update:model-value="container.voyageNumber = ($event as string || undefined)" label="航次号"
+                    @update:model-value="container.voyageNumber = ($event as string || undefined)" label="航海番号"
                     :readonly="readonly" />
             </q-item-section>
         </q-item>
         <q-item class="col-12">
             <q-item-section>
                 <q-input :model-value="container.arrivalDate"
-                    @update:model-value="container.arrivalDate = ($event as string || undefined)" label="到港日期"
+                    @update:model-value="container.arrivalDate = ($event as string || undefined)" label="入港日"
                     type="date" :readonly="readonly" />
             </q-item-section>
         </q-item>
         <q-item class="col-12">
             <q-item-section>
                 <q-input :model-value="container.clearanceDate"
-                    @update:model-value="container.clearanceDate = ($event as string || undefined)" label="清关日期"
+                    @update:model-value="container.clearanceDate = ($event as string || undefined)" label="通関日"
                     type="date" :readonly="readonly" />
             </q-item-section>
         </q-item>
         <q-item class="col-12">
             <q-item-section>
                 <q-input :model-value="container.dischargeDate"
-                    @update:model-value="container.dischargeDate = ($event as string || undefined)" label="卸货日期"
+                    @update:model-value="container.dischargeDate = ($event as string || undefined)" label="荷揚げ日"
                     type="date" :readonly="readonly" />
             </q-item-section>
         </q-item>
         <q-item class="col-12">
             <q-item-section>
                 <q-input :model-value="container.returnDate"
-                    @update:model-value="container.returnDate = ($event as string || undefined)" label="空箱归还日期"
+                    @update:model-value="container.returnDate = ($event as string || undefined)" label="空コンテナ返却日"
                     type="date" :readonly="readonly" />
             </q-item-section>
         </q-item>
         <q-item class="col-12">
             <q-item-section>
-                <q-select :model-value="container.status" @update:model-value="container.status = $event" label="状态"
+                <q-select :model-value="container.status" @update:model-value="container.status = $event" label="状態"
                     :readonly="readonly" :options="[
-                        { label: '在途', value: 'shipping' },
-                        { label: '已到港', value: 'arrived' },
-                        { label: '清关中', value: 'customsClearance' },
-                        { label: '卸货中', value: 'discharging' },
-                        { label: '已卸货', value: 'discharged' },
-                        { label: '空箱', value: 'empty' },
-                        { label: '已归还', value: 'returned' },
-                        { label: '已取消', value: 'canceled' },
+                        { label: '輸送中', value: 'shipping' },
+                        { label: '入港済み', value: 'arrived' },
+                        { label: '通関中', value: 'customsClearance' },
+                        { label: '荷揚げ中', value: 'discharging' },
+                        { label: '荷揚げ完了', value: 'discharged' },
+                        { label: '空コンテナ', value: 'empty' },
+                        { label: '返却済み', value: 'returned' },
+                        { label: 'キャンセル', value: 'canceled' },
                     ]" emit-value map-options />
             </q-item-section>
         </q-item>
